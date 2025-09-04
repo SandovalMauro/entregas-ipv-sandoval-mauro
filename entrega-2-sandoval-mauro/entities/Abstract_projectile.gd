606713c6f922c1d218e93f842b5f0954a28a5b8b
Mauro Sandoval <mgsandoval2021@gmail.com>
2025-09-04 09:20:05 -0300

    desafio 2

--- entrega-2-sandoval-mauro/entities/Abstract_projectile.gd
+++ entrega-2-sandoval-mauro/entities/Abstract_projectile.gd
@@ -10,9 +10,9 @@ var direction:Vector2
 func _ready():
 	set_physics_process(false)
 	
-func set_starting_values(starting_position:Vector2, direction:Vector2):
+func set_starting_values(starting_position:Vector2, directionFire:Vector2):
 	global_position = starting_position
-	self.direction = direction
+	self.direction = directionFire
 	$Timer.start()
 	set_physics_process(true)
 
